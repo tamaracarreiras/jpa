fdfe94c475887368e886136740818af327942d75
Daniel Facciolo Pires <daniel@unifacef.edu.br>
2019-08-27 22:34:13 -0300

    até o passo 7

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,7 +1,11 @@
 package br.com.unifacef.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.unifacef.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
@@ -12,4 +16,15 @@ public class MeuPrimeiroProjetoSpringApplication {
 		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente:" + senhaEncoded);
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", 
+					senhaEncoded));
+		};
+	}
 }
